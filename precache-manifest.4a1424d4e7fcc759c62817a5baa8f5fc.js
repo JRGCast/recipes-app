f8bd0f9072c8ef2bab0e5bddef3c04cef5a474b9
JRGCast <joao.ralph1@gmail.com>
2021-08-28 23:46:44 -0300

    Updates

---
+++ precache-manifest.4a1424d4e7fcc759c62817a5baa8f5fc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e262ba000ab12250d5f9419bcf1563c",
+    "url": "/recipes-app/index.html"
+  },
+  {
+    "revision": "cf8f7a9bbc6712f3fca3",
+    "url": "/recipes-app/static/css/2.95258f03.chunk.css"
+  },
+  {
+    "revision": "aca8ade1dab3ab521ca3",
+    "url": "/recipes-app/static/css/main.81e9bb4e.chunk.css"
+  },
+  {
+    "revision": "cf8f7a9bbc6712f3fca3",
+    "url": "/recipes-app/static/js/2.a6dc723f.chunk.js"
+  },
+  {
+    "revision": "737926aa5f30acaedb866e79ac0007c9",
+    "url": "/recipes-app/static/js/2.a6dc723f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca8ade1dab3ab521ca3",
+    "url": "/recipes-app/static/js/main.66b1316c.chunk.js"
+  },
+  {
+    "revision": "25993f9b630f63b44851",
+    "url": "/recipes-app/static/js/runtime-main.1228032d.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/recipes-app/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/recipes-app/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/recipes-app/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/recipes-app/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/recipes-app/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/recipes-app/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/recipes-app/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/recipes-app/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
